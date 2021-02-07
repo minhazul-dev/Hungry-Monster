992e2c5ef89c5c644c2e7d8d6c6583eccacdaaac
minhazulmoon-rgb <69197671+minhazulmoon-rgb@users.noreply.github.com>
2021-02-07 17:17:22 +0600

    second Final Push

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
-const ingredientsBtn = document.getElementById('ingredientsBtn')
-// ingredientsBtn.addEventListener('click', getMealRecipe)
-
-
-
-
-searchBtn.addEventListener('click', getMealList)
-// ingredientsBtn.addEventListener('click', getMealRecipe)
-
-
-// mealList.addEventListener('click', getMealRecipe)
+const ingredientsBtn = document.getElementById('ingredientsBtn');
+const showIngredients = document.getElementById('showIngredients');
+searchBtn.addEventListener('click', getMealList);
 
 
 function getMealList() {
@@ -21,46 +13,37 @@ function getMealList() {
             let allMeals = ""
             if (data.meals) {
                 data.meals.forEach(meals => {
+
                     allMeals += `<div class = "meal-item" data-id = "${meals.idMeal}">
-                        <div class = "meal-img">
-                            <img src = "${meals.strMealThumb}" alt = "food">
+                     <div id="showIngredients" onclick="getMealRecipe('${meals.strMeal}')" class = "meal-img">
+                       <img src = "${meals.strMealThumb}" alt = "food">
                         </div>
-                        <div class = "meal-name">
-                            <h3>${meals.strMeal}</h3>
-                            
-                 <button id="ingredientsBtn" onclick="getMealRecipe('${meals.strMeal}')"> Show Ingredients </button>
-                            </div>                         
-                    </div> `
-
+                    <div class = "meal-name">
+                          <h3>${meals.strMeal}</h3>
+                    </div>
+                </div> `
                 });
-
-
-            }
-            // if(data.meals==""){
-            //     allMeals = `Please type a Keyword`;
-            //     mealList.classList.add('notFounds');
-            // }
-            else {
+            }           
+          else {
                 allMeals = `Please use a Proper Keyword`;
                 mealList.classList.add('notFound');
             }
             mealList.innerHTML = allMeals;
-        })
+        });
 
-}
+};
 
 const getMealRecipe = ingredients => {
 
-    // console.log(ingredients);
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${ingredients}`
-    // console.log(url);
+  
     fetch(url)
         .then(res => res.json())
         .then(data => renderIngredientsInfo(data.meals[0]))
 }
 
 const renderIngredientsInfo = meals => {
-    // console.log(meals);
+   
     const ingDiv = document.getElementById('showIngredients')
     ingDiv.innerHTML = `
 
@@ -79,6 +62,6 @@ const renderIngredientsInfo = meals => {
     
     `
 
-}
+};
 
 
