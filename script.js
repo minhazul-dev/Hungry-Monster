ed5b76e19715d309f5df03eec201a7db8e6a29e9
minhazulmoon-rgb <69197671+minhazulmoon-rgb@users.noreply.github.com>
2021-02-08 22:23:55 +0600

    modified a little

--- script.js
+++ script.js
@@ -7,48 +7,54 @@ searchBtn.addEventListener('click', getMealList);
 
 function getMealList() {
     let searchInputTxt = document.getElementById('search-input').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputTxt}`)
-        .then(res => res.json())
-        .then(data => {
-            let allMeals = ""
-            if (data.meals) {
-                data.meals.forEach(meals => {
+    if (searchInputTxt === "") {
+        const alerts = document.getElementById('alert');
+         alerts.innerHTML =`
+                      <h1> Please Type Proper Keyword And Search Again <h1>
+         `
 
-                    allMeals += `<div class = "meal-item" data-id = "${meals.idMeal}">
-                     <div id="showIngredients" onclick="getMealRecipe('${meals.strMeal}')" class = "meal-img">
-                       <img src = "${meals.strMealThumb}" alt = "food">
-                        </div>
-                    <div class = "meal-name">
-                          <h3>${meals.strMeal}</h3>
+        
+      
+    }
+    else {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputTxt}`)
+            .then(res => res.json())
+            .then(data => {
+                let allMeals = ""
+                if (data.meals) {
+                    data.meals.forEach(meals => {
+
+                        allMeals += `<div class = "meal-item" data-id = "${meals.idMeal}">
+                 <div id="showIngredients" onclick="getMealRecipe('${meals.strMeal}')" class = "meal-img">
+                   <img src = "${meals.strMealThumb}" alt = "food">
                     </div>
-                </div> `
-                });
-            } 
-            // if(data.meals===" ") 
-            //     {
-            //         allMeals = `Please use a Proper Keyword`;
-            //         mealList.classList.add('notFound');
-            //     }        
-          else {
-                allMeals = `Please use a Proper Keyword`;
-                mealList.classList.add('notFound');
-            }
-            mealList.innerHTML = allMeals;
-        });
+                <div class = "meal-name">
+                      <h3>${meals.strMeal}</h3>
+                </div>
+            </div> `
+                    });
+                }      
+                else {
+                    allMeals = `Please use a Proper Keyword`;
+                    mealList.classList.add('notFound');
+                }
+                mealList.innerHTML = allMeals;
+            });
+    }
 
 };
 
 const getMealRecipe = ingredients => {
 
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${ingredients}`
-  
+
     fetch(url)
         .then(res => res.json())
         .then(data => renderIngredientsInfo(data.meals[0]))
 }
 
-const renderIngredientsInfo = meals => {
-   
+const renderIngredientsInfo = meals =>{
+
     const ingDiv = document.getElementById('showIngredients')
     ingDiv.innerHTML = ` <img id="ingredientsImg" src = "${meals.strMealThumb}" alt = "food">
       
@@ -65,7 +71,6 @@ const renderIngredientsInfo = meals => {
      <li>${meals.strIngredient11}<li>
     
     `
-
 };
 
 
